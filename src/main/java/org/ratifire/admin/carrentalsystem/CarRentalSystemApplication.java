abefe0ee88f4e7aab54068834b09367aad8d364e
shaitan <shaitanaleksender@gmail.com>
2026-03-26 21:23:46 +0100

    Init commit

---
+++ src/main/java/org/ratifire/admin/carrentalsystem/CarRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package org.ratifire.admin.carrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ratifire/admin/carrentalsystem/CarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ratifire.admin.carrentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
